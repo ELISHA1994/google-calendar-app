2ed0afde49db9d36d17f947173eb72959e74d96c
Elisha Bello <elishabello2014@gmail.com>
2021-09-30 16:50:56 +0100

    Deployment

--- src/Unauthenticated-App.js
+++ src/Unauthenticated-App.js
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import Login from "./components/Login";
 import './Unauthenticated-App.css'
-
+// UnauthenticatedApp
 function UnauthenticatedApp({ setAuth }) {
     return (
         <div className="App">
